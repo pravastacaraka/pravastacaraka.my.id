b71754cd64b3bf1cde817108e5ac3e4df82fa147
Pravasta Caraka <43958791+pravastacaraka@users.noreply.github.com>
2021-08-07 18:27:05 +0700

    feat: add chakra-ui (#5)
    * feat: add chakra ui package
    * fix: meta viewport

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { Box, ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <Box as="main" pt={{ base: 16, md: 32 }} pb={{ base: 24, md: 16 }}>
+        <Component {...pageProps} />
+      </Box>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
