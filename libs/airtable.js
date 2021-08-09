24ec3fe89764f995040c44f9bd43ffc870fce290
Pravasta Caraka <43958791+pravastacaraka@users.noreply.github.com>
2021-08-09 16:38:42 +0700

    build: add airtable integration (#9)
    * build: add airtable integration
    * fix: add export for `getTable` function
    * style: update `.env.example`

---
+++ libs/airtable.js
@@ -0,0 +1,25 @@
+import Airtable from "airtable";
+
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
+
+// gets the data we want and puts it into variables
+const minifyRecord = (record) => {
+  return {
+    id: record.id,
+    fields: record.fields,
+  };
+};
+
+// maps over the records, calling minifyRecord, giving us required data
+const getMinifiedRecords = (records) => {
+  return records.map((record) => minifyRecord(record));
+};
+
+async function getTable(table) {
+  const records = await base(table).select({}).all();
+  const minifiedRecords = await getMinifiedRecords(records);
+
+  return minifiedRecords;
+}
+
+export { getTable };
