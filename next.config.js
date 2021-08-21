13d9c7c46af6e2c98ac14033b2aade85d59a8a42
Pravasta Caraka <43958791+pravastacaraka@users.noreply.github.com>
2021-08-22 00:57:11 +0700

    build(deps): add plaiceholder (#22)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+const { withPlaiceholder } = require("@plaiceholder/next");
+
+module.exports = withPlaiceholder({
   reactStrictMode: true,
   images: {
     domains: ["dl.airtable.com"],
   },
-};
+});
