517d7ceb54af4794f990e7a38e106643d37b68bf
Pravasta Caraka <43958791+pravastacaraka@users.noreply.github.com>
2023-01-05 09:52:01 +0700

    chore(deps): update some dependencies and fix loop key (#123)

--- app/achievements/Awards.js
+++ app/achievements/Awards.js
@@ -29,44 +29,42 @@ function Awards({ data }) {
           }
 
           return (
-            <>
-              <Stack key={year} spacing={4}>
-                <Heading as="h3" fontSize={{ base: "lg", md: "xl" }} fontWeight="semibold">
-                  {year}
-                </Heading>
-                <List>
-                  {awardsGroupedByYear[year].map((award) => {
-                    if (countAward >= 4 && !showAwards) {
-                      showDivider = false;
-                      return;
-                    }
+            <Stack key={year} spacing={4} mb={4}>
+              <Heading as="h3" fontSize={{ base: "lg", md: "xl" }} fontWeight="semibold">
+                {year}
+              </Heading>
+              <List>
+                {awardsGroupedByYear[year].map((award) => {
+                  if (countAward >= 4 && !showAwards) {
+                    showDivider = false;
+                    return;
+                  }
 
-                    countAward++;
+                  countAward++;
 
-                    return (
-                      <ListItem key={award.id} mb={4} ml={2}>
-                        <Flex align="center" mb={2}>
-                          <Icon as={HiOutlineBadgeCheck} color={iconColor} mr={2} />
-                          <Text fontWeight="semibold">{award.title}</Text>
+                  return (
+                    <ListItem key={award.id} ml={2}>
+                      <Flex align="center" mb={2}>
+                        <Icon as={HiOutlineBadgeCheck} color={iconColor} mr={2} />
+                        <Text fontWeight="semibold">{award.title}</Text>
+                      </Flex>
+                      <Text fontSize={{ base: "xs", md: "sm" }} mb={2} ml={6} noOfLines={2}>
+                        {award.desc}
+                      </Text>
+                      {award.news_url && (
+                        <Flex align="center" fontSize={{ base: "xs", md: "sm" }} ml={6} noOfLines={1}>
+                          <Icon as={HiOutlineNewspaper} mr={2} />
+                          <NextChakraLink href={award.news_url} isExternal>
+                            {award.news_url}
+                          </NextChakraLink>
                         </Flex>
-                        <Text fontSize={{ base: "xs", md: "sm" }} mb={2} ml={6} noOfLines={2}>
-                          {award.desc}
-                        </Text>
-                        {award.news_url && (
-                          <Flex align="center" fontSize={{ base: "xs", md: "sm" }} ml={6} noOfLines={1}>
-                            <Icon as={HiOutlineNewspaper} mr={2} />
-                            <NextChakraLink href={award.news_url} isExternal>
-                              {award.news_url}
-                            </NextChakraLink>
-                          </Flex>
-                        )}
-                      </ListItem>
-                    );
-                  })}
-                </List>
-              </Stack>
+                      )}
+                    </ListItem>
+                  );
+                })}
+              </List>
               {showDivider && Object.keys(awardsGroupedByYear).length - 1 !== yearIdx && <Divider mb={4} />}
-            </>
+            </Stack>
           );
         })}
       {showAwards ? (
