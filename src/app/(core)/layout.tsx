20d115805fb31ffce25b05380f8e03b486636a5a
Pravasta Caraka <43958791+pravastacaraka@users.noreply.github.com>
2024-07-23 08:18:35 +0700

    feat: add more pages and fix some pages (#226)

--- src/app/(core)/layout.tsx
+++ src/app/(core)/layout.tsx
@@ -1,30 +1,24 @@
-import { _app_metadata, _app_viewport } from "config/app.config";
 import { Footer, Header } from "ui/common/components/layout";
 import MobileMenu from "ui/common/components/layout/MobileMenu/MobileMenu";
-import ChakraWrapper, { Container } from "ui/common/providers/theme.provider";
+import { Container } from "ui/common/providers/theme.provider";
 import { Analytics } from "ui/common/providers/vercel.provider";
 
-export const metadata = _app_metadata;
-export const viewport = _app_viewport;
-
-export default function RootLayout({
+function CoreLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        <ChakraWrapper>
-          <Header />
-          <Container as="main" display="grid" px={4} py={{ base: 4, md: 8 }}>
-            {children}
-            <Analytics />
-          </Container>
-          <Footer />
-          <MobileMenu />
-        </ChakraWrapper>
-      </body>
-    </html>
+    <>
+      <Header />
+      <Container as="main" display="grid" px={4} py={{ base: 4, md: 8 }}>
+        {children}
+        <Analytics />
+      </Container>
+      <Footer />
+      <MobileMenu />
+    </>
   );
 }
+
+export default CoreLayout;
